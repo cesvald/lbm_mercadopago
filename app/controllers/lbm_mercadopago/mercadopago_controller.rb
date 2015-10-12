0d08c086aaab22b69df428faaaabdc8de051cef3
Cesar Valderrama <valderramago@gmail.com>
2015-10-12 13:17:03 -0500

    updated success and failure url for response

--- app/controllers/lbm_mercadopago/mercadopago_controller.rb
+++ app/controllers/lbm_mercadopago/mercadopago_controller.rb
@@ -10,7 +10,7 @@ module LbmMercadopago
   	def review
       backer = current_user.backs.not_confirmed.find params[:id]
       if backer
-        backer.update_attribute :payment_method, 'MercadoPago'
+        #backer.update_attribute :payment_method, 'MercadoPago'
         
         #$mp = MercadoPago.new('1568386747490384', '5Z2hY446SdjpFPYGd8MSjj5UgkgdiNZc')
         $mp = MercadoPago.new('7422021614487510', 'BCTEIO9Qrsi40b26ruvIlmkYKPt2UEyG')
@@ -18,13 +18,13 @@ module LbmMercadopago
     		preference_data = {
           items: [
               {
+              	#title: 'probando',
                 title: t('payulatam_description', scope: SCOPE, :project_name => backer.project.name, :value => backer.display_value),
-                #title: 'probando',
                 quantity: 1,
-                unit_price: backer.value,
                 #unit_price: 25000,
-                currency_id: t('number.currency.format.unit')
+                unit_price: backer.value.to_i,
                 #currency_id: 'COP'
+                currency_id: t('number.currency.format.unit')
               }
           ],
           payer: {
@@ -34,9 +34,9 @@ module LbmMercadopago
             nombre: current_user.name
           },
           back_urls: {
-            success: respond_mercadopago_index_url,
-            pending: respond_mercadopago_index_url,
-            failure: respond_mercadopago_index_url
+            success: respond_mercadopago_url(backer),
+            pending: respond_mercadopago_url(backer),
+            failure: respond_mercadopago_url(backer)
           },
           notification_url: notification_mercadopago_index_url
         }
@@ -46,26 +46,30 @@ module LbmMercadopago
   	end
 
     def respond
-      case params[:collection_status]
-      when 'pending'
-        flash[:failure] = t('mercadopago_pending', scope: SCOPE)
-      when 'approved'
-        flash[:failure] = t('mercadopago_approved', scope: SCOPE)
-      when 'in_process'
-        flash[:failure] = t('mercadopago_in_process', scope: SCOPE)
-      when 'in_mediation'
-        flash[:failure] = t('mercadopago_mediation', scope: SCOPE)
-      when 'rejected'
-        flash[:failure] = t('mercadopago_rejected', scope: SCOPE)
-      when 'cancelled'
-        flash[:failure] = t('mercadopago_cancelled', scope: SCOPE)
-      when 'refunded'
-        flash[:failure] = t('mercadopago_refunded', scope: SCOPE)
-      when 'charged_back'
-        flash[:failure] = t('mercadopago_charged_back', scope: SCOPE)
-      end
-      #redirect_to root_path
-      redirect_to main_app.project_backer_path()
+    	backer = current_user.backs.find params[:id]
+    	case params[:collection_status]
+    	when 'pending'
+    		flash[:failure] = t('mercadopago_pending', scope: SCOPE)
+    	when 'approved'
+    		flash[:failure] = t('mercadopago_approved', scope: SCOPE)
+    	when 'in_process'
+    		flash[:failure] = t('mercadopago_in_process', scope: SCOPE)
+    	when 'in_mediation'
+    		flash[:failure] = t('mercadopago_mediation', scope: SCOPE)
+    	when 'rejected'
+    		flash[:failure] = t('mercadopago_rejected', scope: SCOPE)
+    	when 'cancelled'
+    		flash[:failure] = t('mercadopago_cancelled', scope: SCOPE)
+    	when 'refunded'
+    		flash[:failure] = t('mercadopago_refunded', scope: SCOPE)
+    	when 'charged_back'
+    		flash[:failure] = t('mercadopago_charged_back', scope: SCOPE)
+    	end
+    	if params[:collection_status] == 'approved'
+    		redirect_to main_app.project_backer_path(project_id: backer.project.id, id: backer.id)
+    	else
+    		redirect_to main_app.new_project_backer_path(backer.project)
+    	end
     end
 
     def notification
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ LbmMercadopago::Engine.routes.draw do
 	root :to => 'mercadopago#review'
 	resources :mercadopago do
 	  collection do
-	    get :respond
 	    get :notification
 	    get :create_user
 	    get :pay
 	  end
 	  member do
+	  	get :respond
 	  	get :review
 	  end
 	end
